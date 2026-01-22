d773397cf60c3e9e45d02068fd9870fa89056ac4
nitishkumarshah57 <nitishkumarshah57@gmail.com>
2026-01-22 20:01:48 +0530

    Day 01: Python variables practice

---
+++ month01_foundation/Day01_Variables/variables.py
@@ -0,0 +1,24 @@
+# Day 01 - Variables
+# Topic: Python Variables
+# Author: Nitish Shah
+
+# Integer variable
+age = 19
+
+# Float variable
+height = 5.9
+
+# String variable
+name = "Nitish"
+
+# Boolean variable
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
+
+# Changing value
+age = age + 1
+print("Updated Age:", age)
