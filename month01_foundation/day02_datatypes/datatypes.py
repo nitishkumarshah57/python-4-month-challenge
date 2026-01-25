0314d3cb7aa010501a308f6b3139e554c075179f
nitishkumarshah57 <nitishkumarshah57@gmail.com>
2026-01-25 13:58:51 +0530

    Day 02: Python data types

---
+++ month01_foundation/day02_datatypes/datatypes.py
@@ -0,0 +1,12 @@
+# Day 02 - Data Types
+# Topic: int, float, string, boolean, type()
+
+age = 19              # int
+height = 5.9          # float
+name = "Nitish"       # string
+is_student = True     # boolean
+
+print(age, type(age))
+print(height, type(height))
+print(name, type(name))
+print(is_student, type(is_student))
