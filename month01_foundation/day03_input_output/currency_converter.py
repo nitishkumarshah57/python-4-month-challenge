0207c2247f09713ad44f080acfa69b0de1d5171d
nitishkumarshah57 <nitishkumarshah57@gmail.com>
2026-01-26 17:55:03 +0530

    Day 03: currency converter

--- month01_foundation/day03_input_output/currency_converter.py
+++ month01_foundation/day03_input_output/currency_converter.py
@@ -1,4 +1,12 @@
 # Day 03 - variables, user input, math & type conversion
-# Topic : converting the currency
+# Topic : converting the currency - 1 USD = 0.85 Euros & 1 USD = 110 Yen
 
-budget=
+
+budget = float(input("what is your total budget : "))
+budget_europe = budget/2
+budget_japan = budget/2
+rate_euro = 0.85
+rate_yen = 110
+budget_euro = budget_europe*rate_euro
+budget_yen = budget_japan*rate_yen
+print(f" budget for europe in euro is {budget_euro}, budget for japan in yen is {budget_yen}")
