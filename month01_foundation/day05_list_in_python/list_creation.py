447a24530403c4f04b3875328bfe015da8d3ac91
nitishkumarshah57 <nitishkumarshah57@gmail.com>
2026-02-21 15:23:45 +0530

    list practice in python

---
+++ month01_foundation/day05_list_in_python/list_creation.py
@@ -0,0 +1,7 @@
+# Day 05: list in python
+# Topic: list creation, indexing, negative indexing
+item1 = input("enter your first item:")
+item2 = input("enter your second item:")
+item3 = input("enter your third item:")
+backpack = [item1, item2, item3]
+print(f"first item is {backpack[0]}and last item is {backpack[-1]}")
