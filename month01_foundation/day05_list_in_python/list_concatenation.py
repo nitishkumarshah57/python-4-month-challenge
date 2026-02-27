bb1482a3845288a2e44a3c84ad0fea668643bd0c
nitishkumarshah57 <nitishkumarshah57@gmail.com>
2026-02-27 22:46:53 +0530

    day05: list concatenation

---
+++ month01_foundation/day05_list_in_python/list_concatenation.py
@@ -0,0 +1,9 @@
+# Day 05: list in python
+# Topic: list concatenation(+) and list slicing
+
+party_mix = ["Hookah bar", "Badtameez Dil", "Aankh Marey"]
+chill_mix = ["Bulleya", "Dil Diya Gallan", "Ishq Hua"]
+master_playlist = party_mix + chill_mix
+print(master_playlist)
+short_set = master_playlist[-5:-2]
+print(short_set)
