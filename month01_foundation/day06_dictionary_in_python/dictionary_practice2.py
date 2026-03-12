8ce670d68d6f82dfbb2f373ec3569992d8ed7f33
nitishkumarshah57 <nitishkumarshah57@gmail.com>
2026-03-12 19:46:39 +0530

    day 06 dictionary practice 2

---
+++ month01_foundation/day06_dictionary_in_python/dictionary_practice2.py
@@ -0,0 +1,8 @@
+# Day 06: Dictionary in python
+# Topic: Updating dictionary values and math operators (+=)
+
+employee = {"name":"shiv","age":24,"salary":60000}
+employee["age"] += 1
+employee["salary"] += 5000
+employee["title"] = "senior engineer"
+print(employee)
